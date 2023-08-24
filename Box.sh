9d5838c6b765b93a8e0375791eba56f62cbe5a7c
“yungjr” <pcusb999@gmail.com>
2023-08-24 12:03:40 +0800

    更新

--- Box.sh
+++ Box.sh
@@ -3345,7 +3345,7 @@ while true; do
       12)
       # swap 虛擬內存 SWAP一键安装/卸载脚本
       clear
-        wget -qO- https://raw.githubusercontent.com/yungjr/ChangeSource/main/swap.sh | bash
+        wget -qO- https://raw.githubusercontent.com/yungjr/AWB/main/ChangeSource/swap.sh | bash
         ;;
 
       \
